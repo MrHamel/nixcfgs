bb60ea007a9bae0b28d94b6f0d449cbfbe86919b
Ryan Hamel <ryan@rkhtech.org>
2024-02-10 10:06:52 -0800

    Add my SSH key

--- modules/users/rhamel.nix
+++ modules/users/rhamel.nix
@@ -14,6 +14,8 @@ in
     ];
     hashedPassword = "$6$wk1ToaSAS4AyZ756$tLbS3he803cnjMW/VAuxTNU7qRajAKtZ4jS2GK4jKccZhBH4xXlFpoG02WVWcdCrtSrzcVbx0Io/yTWjpKz4M/";
     isNormalUser = true;
-    #openssh.authorizedKeys.keys = keys.rhamel;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ7GXc68qxUV2b+LXu2AG0rfqmVGq8HVc5ntnPFzP3P"
+    ];
   };
 }
