39df1e2b1c73dff83c27644c67ceebcf14b23bea
Ryan Hamel <ryan@rkhtech.org>
2024-02-12 07:37:59 -0800

    Forgot to push tmux changes

--- users/rhamel/tmux.nix
+++ users/rhamel/tmux.nix
@@ -11,13 +11,22 @@ unbind '"'
 unbind %
 
 # reload config file (change file location to your the tmux.conf you want to use)
-bind r source-file /etc/tmux.conf
+#bind r source-file /etc/tmux.conf
+
+# Easily rename current session
+bind-key r command-prompt "rename-session '%%'"
 
 # Redraw the client (if interrupted by wall, etc)
 bind R refresh-client
 
 # Synchronize command toggle to all panes: [s]
 bind-key s set-window-option synchronize-panes\; display-message "synchronize-panes is now #{?pane_synchronized,on,off}"
+
+# Bring up tree menu: [t]
+bind-key t choose-tree -swZ
+
+# Bring up tree menu and kill tagged sessions upon switch: [k]
+bind-key k choose-tree -wZs "kill-session -t '%%'"
 
 # switch panes using Alt-arrow without prefix
 bind -n M-Left select-pane -L
@@ -40,13 +49,6 @@ setw -g xterm-keys on
 set -sg repeat-time 600                   # increase repeat timeout
 set -s focus-events on
 set -g renumber-windows on
-
-set -g set-titles on          # set terminal title
-
-#set -g display-panes-time 800 # slightly longer pane indicators display time
-#set -g display-time 1000      # slightly longer status messages display time
-
-#set -g status-interval 10     # redraw status line every 10 seconds
 
 set -g status on
 
